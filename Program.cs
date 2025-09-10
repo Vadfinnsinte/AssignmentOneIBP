a5a27fe1a9c8a0865f756baf67dc8a8de7e8d3be
Linda Westerlund <lindawesterlund94@gmail.com>
2025-09-10 09:31:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentOneIBP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
